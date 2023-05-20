abe83b501a3a3741345c3350736b7f7c889286c4
saqib-devops <saqibahmad778866@gmail.com>
2023-05-20 08:34:00 -0400

    Admin: models cretaed

--- src/administration/admins/admin.py
+++ src/administration/admins/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from .models import GuestList, GuestGroup, Guest, InvitationLetter, Invitation, Provider, Table, GuestTable
 
-# Register your models here.
+
+@admin.register(GuestList)
+class GuestListAdmin(admin.ModelAdmin):
+    list_display = ('group_name', 'user')
+    list_filter = ('user',)
+    search_fields = ('group_name',)
+
+
+@admin.register(GuestGroup)
+class GuestGroupAdmin(admin.ModelAdmin):
+    list_display = ('group_name', 'guest_list')
+    list_filter = ('guest_list',)
+    search_fields = ('group_name',)
+
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('guest_name', 'group')
+    list_filter = ('group',)
+    search_fields = ('guest_name',)
+
+
+@admin.register(InvitationLetter)
+class InvitationLetterAdmin(admin.ModelAdmin):
+    list_display = ('group', 'letter_count')
+    list_filter = ('group',)
+    search_fields = ('group__group_name',)
+
+
+@admin.register(Invitation)
+class InvitationAdmin(admin.ModelAdmin):
+    list_display = ('group', 'invitation_count')
+    list_filter = ('group',)
+    search_fields = ('group__group_name',)
+
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ('provider_name', 'service', 'email', 'phone_number', 'total_cost', 'paid', 'user')
+    list_filter = ('user',)
+    search_fields = ('provider_name', 'service', 'email')
+
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('table_name', 'table_type', 'seat_count', 'seats_left', 'user')
+    list_filter = ('user',)
+    search_fields = ('table_name', 'table_type')
+
+
+@admin.register(GuestTable)
+class GuestTableAdmin(admin.ModelAdmin):
+    list_display = ('table', 'guest')
+    list_filter = ('table', 'guest')
+    search_fields = ('guest__guest_name',)
+
+
--- src/administration/admins/models.py
+++ src/administration/admins/models.py
@@ -1,3 +1,80 @@
 from django.db import models
 
+from src.accounts.models import User
+
+
 # Create your models here.
+
+
+class GuestList(models.Model):
+    group_name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.group_name
+
+
+class GuestGroup(models.Model):
+    group_name = models.CharField(max_length=100)
+    guest_list = models.ForeignKey(GuestList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.group_name
+
+
+class Guest(models.Model):
+    guest_name = models.CharField(max_length=100)
+    group = models.ForeignKey(GuestGroup, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.group.group_name
+
+
+class InvitationLetter(models.Model):
+    group = models.ForeignKey(GuestGroup, on_delete=models.CASCADE)
+    letter_count = models.IntegerField()
+
+    def __str__(self):
+        return self.group.group_name
+
+
+class Invitation(models.Model):
+    group = models.ForeignKey(GuestGroup, on_delete=models.CASCADE)
+    invitation_count = models.IntegerField()
+
+    def __str__(self):
+        return self.group.group_name
+
+
+class Provider(models.Model):
+    provider_name = models.CharField(max_length=100)
+    service = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=100)
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    paid = models.DecimalField(max_digits=10, decimal_places=2)
+    attachment = models.FileField(upload_to='attachments/')
+    comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Table(models.Model):
+    table_name = models.CharField(max_length=100)
+    table_type = models.CharField(max_length=100)
+    seat_count = models.IntegerField()
+    seats_left = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class GuestTable(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.guest.group.group_name
